458961002726145856476dc82e0f238be977f375
Simon DENEL <simondenel1@gmail.com>
2018-07-29 22:40:40 +0200

    Updating tests.sh

--- tests/tests.sh
+++ tests/tests.sh
@@ -1,3 +1,6 @@
+#!/usr/bin/env bash
+set -e
+
 rm -f base
 docker save -o base gcr.io/distroless/cc@sha256:2de13d6b26ed1aaa436228ec45b725c408e7d474d8eea9c11f81be8682c374a9
 ./../docker-add-layer base hello-world.sh new-image
