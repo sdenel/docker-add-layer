26c89d9aee98816153ae3c88cc8428e99c9350ea
Simon DENEL <simondenel1@gmail.com>
2018-07-29 23:08:32 +0200

    Fixing tests

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 [![Build Status](https://travis-ci.com/sdenel/docker-add-layer.svg?branch=master)](https://travis-ci.com/sdenel/docker-add-layer)
 
-A python script adding a layer to a Docker image **without** needing tha daemon. Useful, for example, during a CI pipeline inside Docker when you don't want to do Docker in Docker. 
+A python script adding a layer to a Docker image **without** needing tha daemon. Useful, for example, during a CI pipeline inside Docker when you don't want to do Docker in Docker. Requires Python 3.1+ 
--- tests/tests.sh
+++ tests/tests.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 set -e
 
-docker pull gcr.io/distroless/cc
-rm -f base
-docker save -o base gcr.io/distroless/cc
+# TODO: does not work with gcr.io/distroless/cc@sha256:923564f1d33ac659c15edf538b62f716ed436d7cc5f6a9d64460b8affba9ccd9 !
+
+docker pull gcr.io/distroless/cc@sha256:2de13d6b26ed1aaa436228ec45b725c408e7d474d8eea9c11f81be8682c374a9
+rm -f base new-image
+docker save -o base gcr.io/distroless/cc@sha256:2de13d6b26ed1aaa436228ec45b725c408e7d474d8eea9c11f81be8682c374a9
 ./../docker-add-layer base hello-world.sh new-image
-LOADED_IMAGE_ID=`docker load < new-image | cut -d ':' -f 3`
+LOADED_IMAGE_ID=`docker load < new-image | tail -n 1 | cut -d ':' -f 3`
 OUTPUT=`docker run --entrypoint "/hello-world.sh" $LOADED_IMAGE_ID`
 if [[ "$OUTPUT" != "Hello, world!" ]]
 then
